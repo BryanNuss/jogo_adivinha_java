c6dd22369b1919d24d265354c2c464df50f3d6ac
BRYAN KONELL NUSS <bryan_nuss@estudante.sesisenai.org.br>
2024-05-16 11:07:40 -0300

    alterado readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jogo_adivinha_java
 jogo da adivinhação desenvolvido na UC de Lógica de programação do projeto Hellow Blumenau
+
+
+Desenvolvido fase' inicial  do aplicativo e lógica por tras da tomada de decisões do programa.
+
+Utilizando o comando Switch para verificar as dcisões de jogo do usúario , bem como o comando IF
+para validar se o valor digitado pelo usúario é igual ao valor "pensado".
+
+# Proximos features 
+
+Geração de resposta aleatória 
+Poder jogar mais de uma vez 
+Sistema de pontuação 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,52 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner leitura = new Scanner(System.in);
+        String pronto;
+        int resposta;
+        int tentativa;
+
+
+
+
+
+
+
+
+       System.out.println("#######################");
+       System.out.println("##  jogo da Adivinhação");
+       System.out.println("#######################");
+       System.out.println();
+       System.out.println("Regras do jogo");
+       System.out.println("1- Vou pensar em número");
+       System.out.println("2- Você tenta adivinhar qual número estou pensando");
+       System.out.println("3- Te informo se acertou ou não");
+       System.out.println("Está pronto para jogar? (s/n)");
+       pronto = leitura.next();
+
+
+       switch (pronto){
+           case "s":
+               resposta = 6;
+               System.out.println("Qual número entre 0 e 10 estou pensando");
+               tentativa = leitura.nextInt();
+               if(tentativa == resposta){
+               System.out.println("Parabéns, você acertou");
+           }else{
+                   System.out.println("Que pena, você errou");
+
+           }
+               break;
+           case "n":
+               System.out.println("Até mais, então");
+               break;
+           default:
+               System.out.println("Opção inválida!");
+       }
+
     }
 }
